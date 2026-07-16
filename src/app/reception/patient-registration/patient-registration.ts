23ddf1ea96d0f1f6937b344d056cb4a2b06815d1
rjenisha2002-max <r.jenisha2002@gmail.com>
2026-07-16 02:07:07 -0700

    Initial Angular Hospital Management System

---
+++ src/app/reception/patient-registration/patient-registration.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-registration',
+  imports: [],
+  templateUrl: './patient-registration.html',
+  styleUrl: './patient-registration.css',
+})
+export class PatientRegistration {}
